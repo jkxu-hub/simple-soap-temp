7c67716c65b1a1c98627131efe166035b74d5911
Jeff <jkxu@buffalo.edu>
2025-06-29 15:17:35 -0400

    added changes

---
+++ SimpleSoapTemperature/SimpleSoapTemperature/Models/Envelope.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace SimpleSoapTemperature.Models;
+
+[XmlRoot(ElementName = "Envelope", Namespace = "http://schemas.xmlsoap.org/soap/envelope/")]
+public class Envelope
+{
+    [XmlElement(ElementName = "Body", Namespace = "http://schemas.xmlsoap.org/soap/envelope/")]
+    public Body Body { get; set; }
+}
+
+public class Body
+{
+    [XmlElement(ElementName = "FahrenheitToCelsiusResponse", Namespace = "https://www.w3schools.com/xml/")]
+    public FahrenheitToCelsiusResponse FahrenheitToCelsiusResponse { get; set; }
+}
+
+public class FahrenheitToCelsiusResponse
+{
+    [XmlElement(ElementName = "FahrenheitToCelsiusResult", Namespace = "https://www.w3schools.com/xml/")]
+    public double FahrenheitToCelsiusResult { get; set; }
+}
--- SimpleSoapTemperature/SimpleSoapTemperature/Program.cs
+++ SimpleSoapTemperature/SimpleSoapTemperature/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Xml.Serialization;
+using SimpleSoapTemperature.Models;
+
+var soapEndpoint = "https://www.w3schools.com/xml/tempconvert.asmx";
+var temperatureInFarenheit = "87";
+
+var soapEnvelope = $@"<soap:Envelope xmlns:xsi=""http://www.w3.org/2001/XMLSchema-instance"" xmlns:xsd=""http://www.w3.org/2001/XMLSchema"" xmlns:soap=""http://schemas.xmlsoap.org/soap/envelope/"">
+                          <soap:Body>
+                            <FahrenheitToCelsius xmlns=""https://www.w3schools.com/xml/"">
+                              <Fahrenheit>{temperatureInFarenheit}</Fahrenheit>
+                            </FahrenheitToCelsius>
+                          </soap:Body>
+                        </soap:Envelope>";
+
+try
+{
+    // Step 1:  Call the SOAP Endpoint:
+    var client = new HttpClient();
+    var request = new HttpRequestMessage(HttpMethod.Post, soapEndpoint);
+    var content = new StringContent(soapEnvelope, null, "text/xml");
+    request.Content = content;
+    var response = await client.SendAsync(request);
+    response.EnsureSuccessStatusCode();
+    var soapApiResp = await response.Content.ReadAsStringAsync();
+    Console.WriteLine("\n##############################\n");
+    Console.WriteLine(soapApiResp);
+
+    // Step 2: Deserialize the response:
+    Envelope envelope = null;
+    var serializer = new XmlSerializer(typeof(Envelope));
+    using (var reader = new StringReader(soapApiResp))
+    {
+        envelope = (Envelope)serializer.Deserialize(reader);
+    }
+    Console.WriteLine("\n##############################\n");
+    Console.WriteLine($"{temperatureInFarenheit} Farenheit is {envelope?.Body?.FahrenheitToCelsiusResponse?.FahrenheitToCelsiusResult} Celsius");
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.ToString());
+}
